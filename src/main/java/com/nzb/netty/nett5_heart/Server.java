40a641b00198d26895d17da4db9061e2c7cdcc20
NieZhengBing <nzb15555196162@gmail.com>
2018-02-06 16:55:39 +0800

    netty5-heart first commit

---
+++ src/main/java/com/nzb/netty/nett5_heart/Server.java
@@ -0,0 +1,48 @@
+package com.nzb.netty.nett5_heart;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class Server {
+	public void main(String[] args) {
+		ServerBootstrap bootstrap = new ServerBootstrap();
+
+		EventLoopGroup boss = new NioEventLoopGroup();
+		EventLoopGroup worker = new NioEventLoopGroup();
+
+		try {
+			bootstrap.group(boss, worker);
+			bootstrap.channel(NioServerSocketChannel.class);
+
+			bootstrap.childHandler(new ChannelInitializer<Channel>() {
+
+				@Override
+				protected void initChannel(Channel ch) throws Exception {
+					ch.pipeline().addLast(new IdleStateHandler(5, 5, 10));
+					ch.pipeline().addLast(new StringDecoder());
+					ch.pipeline().addLast(new StringEncoder());
+					ch.pipeline().addLast(new ServerHandler());
+				}
+			});
+
+			bootstrap.option(ChannelOption.SO_BACKLOG, 2048);
+			bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+			bootstrap.childOption(ChannelOption.TCP_NODELAY, true);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			boss.shutdownGracefully();
+			worker.shutdownGracefully();
+		}
+	}
+
+}
---
+++ src/main/java/com/nzb/netty/nett5_heart/ServerHandler.java
@@ -0,0 +1,52 @@
+package com.nzb.netty.nett5_heart;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+
+public class ServerHandler extends SimpleChannelInboundHandler<String> {
+
+	@Override
+	public void userEventTriggered(final ChannelHandlerContext ctx, Object evt) throws Exception {
+		if (evt instanceof IdleStateEvent) {
+			IdleStateEvent event = (IdleStateEvent) evt;
+			if (event.state() == IdleState.ALL_IDLE) {
+				ChannelFuture future = ctx.writeAndFlush("close session");
+				future.addListener(new ChannelFutureListener() {
+
+					public void operationComplete(ChannelFuture arg0) throws Exception {
+						ctx.channel().close();
+					}
+				});
+			}
+		}
+		super.userEventTriggered(ctx, evt);
+	}
+
+	@Override
+	protected void messageReceived(ChannelHandlerContext ctx, String msg) throws Exception {
+		System.out.println(msg);
+
+		ctx.channel().writeAndFlush("hi");
+		ctx.writeAndFlush("hi");
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		super.channelActive(ctx);
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		super.channelInactive(ctx);
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		super.exceptionCaught(ctx, cause);
+	}
+
+}
